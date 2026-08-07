4db9af49e42b6590b5b4f5e9817fadc9a1a570e1
Dogancan Karakoc <dogncankrkc@gmail.com>
2026-08-07 23:42:26 +0300

    Update BookingForm.jsx

--- src/components/ui/BookingForm.jsx
+++ src/components/ui/BookingForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import Button from "../common/Button.jsx";
 import HeroImage from "../../assets/images/hero-image.jpg";
 import { useFormik } from "formik";
